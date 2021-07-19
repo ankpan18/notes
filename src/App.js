bfa92bf2a2baaf92603e8b7469cca75a8e5d937a
Ankur Panthri <79756942+ankpan18@users.noreply.github.com>
2021-07-19 20:39:24 +0530

    Third Version-TodoList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   let items=[
@@ -25,21 +26,7 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="todolist">
-          <h1>{title.toUpperCase()}</h1>
-          <ul className="list-unstyled">
-            {items.map(item=> (
-              <li key={item.id} className="ui-state-default">
-                <div className="checkbox">
-                  <label>
-                    <input type="checkbox" value=""/>
-                    {item.text}
-                  </label>
-                </div>
-              </li>
-            )) } 
-          </ul>
-        </div>
+        <TodoList title={title} items={items}/>
         </div>
 
     </div>
