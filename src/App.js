8a3dc9c3ee2f10ac942e71e37f17ca84d5211908
Ankur Panthri <79756942+ankpan18@users.noreply.github.com>
2021-07-19 12:32:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="todolist">
+          <h1>Todos</h1>
+          <ul className="list-unstyled">
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>Take out the trash
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>Buy bread
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Teach penguins to fly</label>
+                            </div>
+                        </li>   
+          </ul>
+        </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
