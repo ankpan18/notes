3848ff88c0a4fb9dcc79e3cc5f3ebae6a860ed9f
Ankur Panthri <79756942+ankpan18@users.noreply.github.com>
2021-07-22 20:54:43 +0530

    More Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './TodoList';
+import TodoList from './components/TodoList';
 
 function App() {
   let items=[
---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import Header from './Header';
+
+function TodoList(props){
+const{title,items}=props;
+return (
+        <div className="todolist">
+          <Header title={title}/>
+          <ul className="list-unstyled">
+            {items.map(item=><TodoItem key={item.id} data={item}/>)}
+          </ul>
+        </div>
+  );
+}
+
+
+
+export default TodoList;
